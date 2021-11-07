dd6ac56253b93a553f2fbca53c961a2122544fae
Ari Fordsham <arifordsham@gmail.com>
2021-11-07 23:26:42 +0000

    Intercept Docker args

--- docker-cert.sh
+++ docker-cert.sh
@@ -1,8 +1,28 @@
-# docker run \
-#   --mount type=bind,source=$CERTFILE,target=/certs/ssl/cert.pem \
-#   --mount type=bind,source=$CERTDIR,target=/certs/ssl/certs \
-#   -e "SSL_CERT_DIR=/certs/ssl/certs" \
-#   -e "SSL_CERT_FILE=/certs/ssl/cert.pem" \
-#   docker-cert
+DEFAULT_CERT_DIR=$(openssl version -d | sed -e "s/^OPENSSLDIR: \"//" -e "s/\"$//")
 
-docker run -ti --mount type=bind,source=/usr/local/share/ca-certificates/netfree-ca.crt,target=/usr/lib/ssl/certs/netfree-ca.pem docker-cert bash
+CAFILE=${SSL_CERT_FILE:-$DEFAULT_CERT_DIR/cert.pem}
+CAPATH=${SSL_CERT_DIR:-$DEFAULT_CERT_DIR/certs}
+
+CONT_CAFILE=/ssl/cert.pem
+CONT_CAPATH=/ssl/certs
+
+# Can't just mount CAPATH, because typically many certificates are symbolic
+# links and symlinks don't work through bindmounts.
+# Instead we iterate the directory and build a list of mount flags to mount each 
+# file individually - bind mount resolves symlinks before mount.
+CERT_MOUNT_FLAGS=$(ls $CAPATH | sed "s|.*|-v $CAPATH/&:$CONT_CAPATH/&|")
+
+case $1 in
+  run)
+    shift
+    docker run \
+      -v $CAFILE:$CONT_CAFILE \
+      -e SSL_CERT_FILE=$CONT_CAFILE \
+      $CERT_MOUNT_FLAGS \
+      -e SSL_CERT_DIR=$CONT_CAPATH \
+        $@
+    ;;
+  *)
+    docker $@
+    ;;
+esac
